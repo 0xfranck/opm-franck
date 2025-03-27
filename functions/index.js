9b00fa352d5fea20dc67da6cfc6df94cd6670f62
0xfranck <105662496+0xfranck@users.noreply.github.com>
2025-03-27 00:27:48 +0000

    redeploy

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
     const content = await context.env.MY_KV.get("opm-franck--latest");
-    const html = content || "<h1>No content available</h1>";
+    const html = content || "<h1>No content available.</h1>";
     return new Response(html, {
       headers: { "Content-Type": "text/html" },
     });
